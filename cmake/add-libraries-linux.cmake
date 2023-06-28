82b97273c5e68157d0ec2d5d8de8435689f27bf5
jeesun <jeesun@sha.kr>
2023-06-28 10:34:14 +0900

    build: update freeimage/opencv libs

--- cmake/add-libraries-linux.cmake
+++ cmake/add-libraries-linux.cmake
@@ -1,11 +1,9 @@
-include_directories("/usr/local/include")
-include_directories("/usr/local/include/opencv4")
 include_directories("${CMAKE_SOURCE_DIR}/3rd_party/glad/include")
 include_directories("${CMAKE_SOURCE_DIR}/3rd_party/glfw3/include")
 include_directories("${CMAKE_SOURCE_DIR}/3rd_party/glm")
 include_directories("${CMAKE_SOURCE_DIR}/3rd_party/freeimage/include")
-
-link_directories("/usr/local/lib")
+include_directories("${CMAKE_SOURCE_DIR}/3rd_party/opencv/include")
 link_directories("${CMAKE_SOURCE_DIR}/3rd_party/glad/lib/linux")
 link_directories("${CMAKE_SOURCE_DIR}/3rd_party/glfw3/lib/linux")
 link_directories("${CMAKE_SOURCE_DIR}/3rd_party/freeimage/lib/linux")
+link_directories("${CMAKE_SOURCE_DIR}/3rd_party/opencv/lib/linux")
